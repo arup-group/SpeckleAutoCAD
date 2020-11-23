7289b1c37a6496ed29eb38f8bfbbc15cf20b8cdf
GLOBAL\George.Aggrey <george.aggrey@arup.com>
2020-11-23 13:19:39 +0000

    Support multiple versions, Add basic installer.

---
+++ Installer/InstallerScript.iss
@@ -0,0 +1,43 @@
+#define AppName "SpeckleAutoCAD"
+#define AppPublisher "Speckle"
+#define AppURL "https://github.com/arup-group/SpeckleAutoCAD"
+#define AppVersion "0.0.1"
+#define ErrorMessage "AutoCAD/Civil3D is not installed on this machine. Setup will now exit."
+
+
+[Setup]
+AppId={{FFE541D3-60E4-4A3E-8224-D0C51F238690}
+AppName={#AppName}
+AppVersion={#AppVersion}
+AppVerName={#AppName} {#AppVersion}
+AppPublisher={#AppPublisher}
+AppPublisherURL={#AppURL}
+AppSupportURL={#AppURL}
+AppUpdatesURL={#AppURL}
+DefaultDirName="C:\ProgramData\SpeckleAutoCAD"
+DisableDirPage=yes
+DefaultGroupName={#AppName}
+DisableProgramGroupPage=yes
+DisableWelcomePage=no
+OutputDir="Build\"
+OutputBaseFilename=SpeckleAutoCADInstaller
+SetupIconFile="Assets\icon.ico"
+Compression=lzma
+SolidCompression=yes
+WizardImageFile="Assets\installer.bmp"
+ChangesAssociations=yes
+; PrivilegesRequired must be "none" to access ProgramData
+PrivilegesRequired=none
+VersionInfoVersion={#AppVersion}
+
+[Languages]
+Name: "english"; MessagesFile: "compiler:Default.isl"
+
+[Components]
+Name: autocad20; Description: Speckle for AutoCAD/Civil3D 2020 WIP;
+Name: autocad21; Description: Speckle for AutoCAD/Civil3D 2021 WIP;
+
+[Files]
+Source: "..\SpeckleAutoCAD2020\Build\Release\*"; DestDir: "{userappdata}\SpeckleAutoCAD\2020\"; Flags: ignoreversion recursesubdirs; Components: autocad20
+Source: "..\SpeckleAutoCAD2021\Build\Release\*"; DestDir: "{userappdata}\SpeckleAutoCAD\2021\"; Flags: ignoreversion recursesubdirs; Components: autocad21
+
---
+++ SpeckleAutoCAD2020/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.Civil.ApplicationServices;
+using SpeckleUiBase;
+using SpeckleAutoCAD.UI;
+
+namespace SpeckleAutoCAD
+{
+    public class Program : IExtensionApplication
+    {
+        public static bool Launched = false;
+        public static SpeckleUiWindow SpeckleWindow;
+
+        #region IExtensionApplication Members
+
+
+
+        public void Initialize()
+        {
+ //           throw new System.Exception("The method or operation is not implemented.");
+        }
+
+        public void Terminate()
+        {
+ //           throw new System.Exception("The method or operation is not implemented.");
+        }
+
+        #endregion  
+
+        [CommandMethod("Speckle")]
+        public void Speckle()
+        {
+            // Gets the current document, and prints some basic information about it
+            // to the command editor window.
+
+            
+
+            CivilDocument doc = Autodesk.Civil.ApplicationServices.CivilApplication.ActiveDocument;
+
+            var bindings = new SpeckleUIBindingsAutoCAD(doc);
+
+            ObjectIdCollection alignments = doc.GetAlignmentIds();
+            ObjectIdCollection sites = doc.GetSiteIds();
+            String docInfo = String.Format("\nHello Speckle!");
+            Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage(docInfo);
+
+            SpeckleWindow = new SpeckleUiWindow(bindings);
+
+            var helper = new System.Windows.Interop.WindowInteropHelper(SpeckleWindow);
+            helper.Owner = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+
+            SpeckleWindow.Show();
+            Launched = true;
+
+        }
+    }
+}
